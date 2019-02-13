bd027baf8028bdef7059dcca85d45f7a168e8ed3
Kai Wu Toh <kwtoh2000@hotmail.com>
2019-02-13 11:48:10 +0800

    Remove unused code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 var countup = false;
 var start = false;
 var totalseconds = 0;
-
+var cards = document.getElementsByClassName("card");
+var numbers = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 var zodiac = [
 	{	
 		m_id :'mouse',
@@ -53,7 +54,6 @@ var zodiac = [
 	}
 ];
 
-var numbers = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
 // Start Game Button
 function startGame() {
@@ -64,9 +64,8 @@ function startGame() {
 	// Shuffle the array
 	shuffle(numbers);
 
-	var elements = document.getElementsByClassName("card");
-	for (var i = 0; i < elements.length; i++) {
-		startCard(elements[i], zodiac[numbers[i]]);
+	for (var i = 0; i < cards.length; i++) {
+		startCard(cards[i], zodiac[numbers[i]]);
 	}
 }
 
@@ -114,9 +113,8 @@ function turn(id) {
 		return start;
 	}
 
-	var elements = document.getElementsByClassName("card");
-	for (var i = 0; i < elements.length; i++) {
-		if (elements[i].textContent !== "🍊") {
+	for (var i = 0; i < cards.length; i++) {
+		if (cards[i].textContent !== "🍊") {
 			return true;
 		}
 	}
@@ -139,116 +137,3 @@ function shuffle(o) {
 	for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
     return o;
 }
-
-/***************************
-PARTY POPPER
-***************************/
-/**
- * Creates a Mo.js party popper animation.
- *
- * @property {string}  selector - DOM selector to attach the animation to
- * @property {boolean} [debug]  - If enabled, add a debug timeline
- */
-
-function partyPopper(selector, debug) {
-	const colors = [
-		'#bea4ff',
-		'#feb535',
-		'#ff6e83',
-		'#58cafe',
-	]
-
-	const flight = {
-		isSwirl: true,
-		swirlSize: 'rand(10, 20)',
-		swirlFrequency: 'rand(1, 3)',
-		direction: [-1, 1],
-		degreeShift: 'rand(-15, 15)',
-		duration: 1200,
-		easing: 'cubic.out',
-		pathScale: 'stagger(.2)',
-	}
-
-	// Confetti shapes
-	const torsade = {
-		shape: 'zigzag',
-		points: 'rand(4, 6)',
-		radius: 40,
-		radiusY: 30,
-		strokeLinecap: 'round',
-		strokeWidth: 8,
-		fill: 'none',
-		stroke: colors,
-		angle: {0: 'rand(-720, 720)'},
-		...flight,
-	}
-
-	const bent = {
-		shape: 'curve',
-		radius: 'rand(25, 35)',
-		radiusY: 15,
-		strokeLinecap: 'round',
-		strokeWidth: 8,
-		fill: 'none',
-		stroke: colors,
-		angle: {0: 'rand(-720, 720)'},
-		...flight,
-	}
-
-	const flake = {
-		shape:'circle',
-		radius: 'rand(5, 10)',
-		fill: colors,
-		...flight,
-	}
-
-	// Bursts
-	const burst = {
-		parent: selector,
-		radius: {0 : 'rand(50, 100)'},
-		count: 'rand(18, 22)',
-		degree: 30,
-	}
-
-	const torsadeBurst = new mojs.Burst({
-		...burst,
-		children: {
-			...torsade,
-		}
-	});
-
-	const bentBurst = new mojs.Burst({
-		...burst,
-		children: {
-			...bent,
-		}
-	});
-
-	const flakeBurst = new mojs.Burst({
-		...burst,
-		children: {
-			...flake,
-		}
-	});
-
-	// Timeline (debug only)
-	if (debug != null) {
-		const timeline = new mojs.Timeline;
-
-		timeline.add(
-			torsadeBurst, 
-			bentBurst, 
-			flakeBurst,
-		);
-
-		new MojsPlayer({
-			add: timeline,
-			isPlaying: true,
-			isRepeat: true,
-		});	
-	} else {
-		torsadeBurst.play()
-		bentBurst.play()
-		flakeBurst.play()
-	}
-};
